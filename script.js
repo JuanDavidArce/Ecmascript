de4e00037a8174708f7a42ecca87ca41dfa6445b
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-11-22 21:30:34 -0500

    Arrow functions and promises

--- script.js
+++ script.js
@@ -71,5 +71,43 @@ console.log(globalVar);
 
 
 const a = "b";
-a='a'; // We can't do this because a is a constant
+// a='a'; // We can't do this because a is a constant
 
+name='oscar';
+age=32;
+
+// es5
+obj ={name:name,age:age};
+
+// es6
+obj2={name,age};
+
+console.log(obj2);
+
+const names = [
+    {name:'oscar', age:32},
+    {name:'yesica',age:27}
+]
+
+let listOfNames = names.map(function(item){
+    console.log(item.name);
+});
+
+let listOfNames2= names.map(item=>console.log(item.name));
+
+const square = num=>num*num;
+
+const helloPromise= () => {
+    return new Promise((resolve,reject)=>{
+        if(false){
+            resolve("Hey");
+        }
+        else{
+            reject("Ups!!");
+        }
+    })
+}
+
+helloPromise()
+.then(response=> console.log(response))
+.catch(error=>console.log(error));
