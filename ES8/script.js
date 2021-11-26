cd137c90daa70c62e5053f49f9c8937f25ea2107
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-11-25 20:46:27 -0500

    async and await

--- ES8/script.js
+++ ES8/script.js
@@ -15,3 +15,37 @@ const string='hello';
 console.log(string.padStart(7,'hi'));
 console.log(string.padEnd(12,' -------'));
 console.log('food'.padEnd(12,' -------'));
+
+
+const helloWorld=()=> {
+    return new Promise((resolve,reject)=>{
+        (false)
+            ?setTimeout(()=>resolve('hello world'),300)
+            : reject(new Error('Test Error'))
+    })
+};
+
+
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsync();
+
+
+const anotherFunction = async ()=> {
+    try 
+    {
+      const hello= await helloWorld();
+      console.log(hello);  
+    } 
+    catch (error) 
+    {
+        console.log(error);
+    };
+}
+
+anotherFunction();
+
